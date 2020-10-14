77b85f044fa928e77da00fbc5bdd4347e56db99a
Carlos A. Lobo <lobor.candres@gmail.com>
2020-10-14 02:08:24 -0400

    Added a new polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,9 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -rq music &
-polybar -rq tray &
-polybar -rq ws &
-
+polybar -rq top &
+polybar -rq bottom &
 
 echo "Polybar launched..."
